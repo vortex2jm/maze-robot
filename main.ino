0ee22891729f2d9c583eb92c462926c678d397dc
KPMGE <jesuskevin254@gmail.com>
2023-11-20 20:26:48 -0300

    :sparkles: implement pid function and its parameters

--- main.ino
+++ main.ino
@@ -24,3 +24,41 @@
 #define DB6 25
 #define DB5 24
 #define DB4 23
+
+#define KP 
+#define KI 
+#define KD 
+#define INPUT_PIN 
+#define OUT_PIN 
+
+double integral = 0;
+double dt = 0;
+double last_time = 0;
+double desired_output = 5;
+
+double pid(double error);
+
+void setup() {
+}
+
+void loop() {
+  double now = millis();
+  dt = (now - last_time) / 1000.0;
+  last_time = now;
+
+  double actual_output = map(analogRead(INPUT_PIN), 0, 1024, 0, 255);
+  double error = desired_output - actual_output;
+  output = pid(error);
+  analogWrite(OUT_PIN, output);
+
+  delay(300);
+}
+
+double pid(double error) {
+  double proportional = error;
+  integral += error * dt; 
+  double derivative = (error - previous) / dt;
+  previous = error;
+  double output = (KP * proportional) + (KI * integral) + (KD * derivative);
+  return output;
+}
