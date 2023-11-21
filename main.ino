e6bf23e83a5daa42c2d2cb8f8e6620e3defd45fa
KPMGE <jesuskevin254@gmail.com>
2023-11-21 00:13:27 -0300

    :page_facing_up: add comments

--- main.ino
+++ main.ino
@@ -1,25 +1,41 @@
-#define SLC 3
-#define SR 4
-#define SC 5
-#define SL 6
-#define SRC 7
+#define CENTER_LEFT_SENSOR 3 
+#define RIGHT_SENSOR 4
+#define CENTER_VERTICAL_SENSOR 5
+#define LEFT_SENSOR 6
+#define CENTER_RIGHT_SENSOR 7
+
+// located on H bridge
 #define E_CHI 8
 #define CHA_MI 9
 #define E_CH2 10
 #define CHA_M2 11
+
 #define BUZZER 12
-#define RL_LCD 13
 #define SERVO 15
 #define MOSI 37
-#define SCL_MPU 36
 #define TX0 35
 #define RX0 34
-#define SDA_MPU 33
-#define MISO_ECHO 31
 #define CLK 30
+
+// i2c acelerometer & giroscope
+#define SDA_MPU 33
+#define SCL_MPU 36
+
+// enable sensors
 #define CS_SENSORS 29
-#define RS 28
+
+
+// general purpose enable
 #define EN_TRIG 27
+
+#define MISO_ECHO 31
+
+// lcd register select
+#define RS 28
+// power lcd
+#define RL_LCD 13
+
+// data bits lcd
 #define DB7 26
 #define DB6 25
 #define DB5 24
@@ -64,11 +80,11 @@ double pid(double error) {
 // the mask will be like: 000<SLC><SR><SC><SL><SRC>
 unsigned char get_line_sensors_bitmask() {
   // change to analogRead
-  int slc_val = digitalRead(SLC);
-  int sr_val = digitalRead(SR);
-  int sc_val = digitalRead(SC);
-  int sl_val = digitalRead(SL);
-  int src_val = digitalRead(SRC);
+  int slc_val = digitalRead(CENTER_LEFT_SENSOR);
+  int sr_val = digitalRead(RIGHT_SENSOR );
+  int sc_val = digitalRead(CENTER_VERTICAL_SENSOR);
+  int sl_val = digitalRead(LEFT_SENSOR);
+  int src_val = digitalRead(CENTER_RIGHT_SENSOR );
   
   return (slc_val << 4) | (sr_val << 3) | (sc_val << 2) | (sl_val << 1) | src_val;
 }
