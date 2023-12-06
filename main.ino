aa4a2a354ed170c143609ef9ec1d6ad8a7bb24f5
vortex2jm <joaopaulomoura5328@gmail.com>
2023-12-06 17:20:24 -0300

    :construction: Testing new Robot class

--- main.ino
+++ main.ino
@@ -1,65 +1,75 @@
-// Sensors
+#include <robot.h>
+
+// Line sensors pins
 #define CENTER_RIGHT_SENSOR 36
 #define RIGHT_SENSOR 39
 #define CENTER_VERTICAL_SENSOR 34
 #define LEFT_SENSOR 35
 #define CENTER_LEFT_SENSOR 32
 
-// Right engine - 128 -> 255
+// Engines pins
 #define EN_1 33
 #define CHA_M1 25
-
-// Left engine - 128 -> 0
 #define EN_2 26
 #define CHA_M2 27
 
-// Medium value between light sensors values
-#define TRESHOLD 2000
+// Ultrasonic pins
+#define ECHO_PIN  19
+#define TRIGGER_PIN 16
 
-#define KP 0.005
+Robot robot(
+  CENTER_VERTICAL_SENSOR,
+  LEFT_SENSOR,
+  RIGHT_SENSOR,
+  CENTER_LEFT_SENSOR,
+  CENTER_RIGHT_SENSOR,
+  EN_1,
+  EN_2,
+  CHA_M1,
+  CHA_M2,
+  ECHO_PIN,
+  TRIGGER_PIN
+);
 
-int center_vertical_sensor_value=0;
-int left_sensor_value=0;
-int right_sensor_value=0;
-int center_left_sensor_value=0; 
-int center_right_sensor_value=0;
-
-//SETUP==================================================//
 void setup() {
-  // Setting up pins
-  pinMode(CENTER_LEFT_SENSOR,INPUT);
-  pinMode(RIGHT_SENSOR,INPUT);
-  pinMode(CENTER_VERTICAL_SENSOR,INPUT);
-  pinMode(LEFT_SENSOR,INPUT);
-  pinMode(CENTER_RIGHT_SENSOR,INPUT);
-  pinMode(CHA_M1, OUTPUT);
-  pinMode(EN_1, OUTPUT);
-  pinMode(CHA_M2, OUTPUT);
-  pinMode(EN_2, OUTPUT);
-
-  // Enabling engines
-  digitalWrite(EN_1, HIGH);
-  digitalWrite(EN_2, HIGH);
-
   Serial.begin(9600);
+  robot.setup();
 }
 
-//LOOP==================================================//
 void loop() {
-  center_vertical_sensor_value = analogRead(CENTER_VERTICAL_SENSOR);
-  left_sensor_value = analogRead(LEFT_SENSOR);
-  right_sensor_value = analogRead(RIGHT_SENSOR);
-  center_left_sensor_value = analogRead(CENTER_LEFT_SENSOR);
-  center_right_sensor_value = analogRead(CENTER_RIGHT_SENSOR); 
+  robot.read_line_sensors();
+  RobotState state = robot.check_line_sensors_states();
+  // Serial.println(state);
 
-  Serial.println(left_sensor_value, right_sensor_value);
+  switch (state) {
+    case GO_FORWARD:
+      robot.go_forward();
+    break;
 
-  analogWrite(CHA_M1, 160 + (KP * left_sensor_value));
-  analogWrite(CHA_M2,86 - (KP * right_sensor_value));
-  delay(100);
+    case FOUR_WAY_CROSSING:
+      robot.rotate_right_90deg();
+      robot.go_forward();
+      delay(500);
+    break;
 
-  if(center_left_sensor_value > TRESHOLD || center_right_sensor_value > TRESHOLD){
-    digitalWrite(EN_1, LOW);
-    digitalWrite(EN_2, LOW);
+    case THREE_WAY_CROSSING:
+      robot.rotate_right_90deg();
+      robot.go_forward();
+      delay(500);
+    break;
+
+    case TURN_LEFT:
+      robot.rotate_right_90deg();
+      robot.go_forward();
+      delay(500);
+    break;
+    case TURN_RIGHT:
+      robot.rotate_right_90deg();
+      robot.go_forward();
+      delay(500);
+    break;
+
+    default:
+      robot.go_forward();
   }
 }
